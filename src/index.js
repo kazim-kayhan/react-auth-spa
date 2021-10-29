876ef9991c001fe2ab546ccab5a0dc9634fae399
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-10-29 23:22:17 +0430

    Add baseURL and headers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './customStyle.css';
+
+axios.defaults.baseURL = 'http://laravel-react-auth.herokuapp.com/api/';
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem( 'token' );
 
 ReactDOM.render(
   <React.StrictMode>
