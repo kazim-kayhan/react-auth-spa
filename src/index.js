b18ae065b9a135a9a4d9433586f1bfaaeb5dcacb
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-10-31 16:13:14 +0430

    Add backend api baseUrl

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './customStyle.css';
 
-// axios.defaults.baseURL = 'https://laravel-react-auth.herokuapp.com/api/';
-axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.baseURL = 'https://laravel-react-auth.herokuapp.com/api/';
+// axios.defaults.baseURL = 'http://localhost:8000/api/';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem( 'token' );
 
 ReactDOM.render(
